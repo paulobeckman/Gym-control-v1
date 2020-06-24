99c7a0ec9749d720132eedfdee00f962d7691128
Paulo Beckman <beckmam.paulo@hotmail.com>
2020-06-24 10:03:53 -0300

    update2406 commit

--- controllers/instructors.js
+++ controllers/instructors.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
-const data = require("./data.json")
-const { age, date } = require('./utils')
+const data = require("../data.json")
+const { age, date } = require('../utils')
 const Intl = require('intl')
 
 //index
@@ -21,6 +21,11 @@ exports.index = function(req,res){
 }
 
 //create
+exports.create = function(req,res){
+    return res.render("instructors/create")
+}
+
+//post
 exports.post = function(req,res){
 
     const keys = Object.keys(req.body)
@@ -90,7 +95,7 @@ exports.edit = function(req, res) {
 
     const instructor = {
         ...foundInstructor,
-        birth: date(foundInstructor.birth)
+        birth: date(foundInstructor.birth).iso
     }
 
     return res.render("instructors/edit", { instructor })
